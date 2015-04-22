04a152d90fbb0bf11b84277002b0fad21b773795
Shu Foujita <osorubeki.fujita@gmail.com>
2015-04-22 14:03:48 +0900

    20150422_1403 Deploying 0.2.0

--- lib/required_files.rb
+++ lib/required_files.rb
@@ -16,19 +16,43 @@ module RequiredFiles
 
     private
 
+    def set_all_files_under_the_top_namespace?
+      @set_all_files_under_the_top_namespace
+    end
+
     def set_array
-      @array << self.class.top_file
+      set_top_file
       set_other_files
-      if @set_all_files_under_the_top_namespace
-        @array += self.class.all_files_under_the_top_namespace
+      set_all_files_under_the_top_namespace
+    end
+
+    def set_top_file
+      _ignored_files = ignored_files
+      _top_file = top_file
+      unless _ignored_files.present? and _ignored_files.include?( _top_file )
+        @array << _top_file
       end
     end
 
     def set_other_files
-      _other_files = self.class.other_files
+      _ignored_files = ignored_files
+      _other_files = other_files
       if _other_files.present?
-        [ _other_files ].flatten.each do |f|
-          set_files(f)
+        _other_files.each do | file |
+          unless _ignored_files.present? and _ignored_files.include?( file )
+            set_files( file )
+          end
+        end
+      end
+    end
+    
+    def set_all_files_under_the_top_namespace
+      if set_all_files_under_the_top_namespace?
+        _ignored_files = ignored_files
+        all_files_under_the_top_namespace.each do | file |
+          unless _ignored_files.present? and _ignored_files.include?( file )
+            @array << file
+          end
         end
       end
     end
@@ -37,7 +61,7 @@ module RequiredFiles
       files.flatten.each do | file |
         filename_without_extension = file.gsub( /\.rb\Z/ , "" )
         filename_with_extension = filename_without_extension + ".rb"
-        if File.exist?( filename_with_extension ) and !( @array.include?( filename_without_extension ) )
+        if ::File.exist?( filename_with_extension ) and !( @array.include?( filename_without_extension ) )
           @array << filename_without_extension
         end
       end
@@ -47,20 +71,49 @@ module RequiredFiles
       set_files( self.class.files_starting_with( *dir_root ) )
     end
 
+    [ :top_file , :all_files_under_the_top_namespace ].each do | class_method_name |
+      eval <<-DEF
+        def #{ class_method_name }
+          self.class.#{ class_method_name }
+        end
+      DEF
+    end
+
+    def other_files
+      other_or_ignored_files( self.class.other_files )
+    end
+
+    def ignored_files
+      other_or_ignored_files( self.class.ignored_files )
+    end
+
+    def other_or_ignored_files( files )
+      if files.present?
+        [ files ].flatten
+      else
+        nil
+      end
+    end
+
     def self.files
       self.new.files
     end
 
     def self.top_file
-      raise "This method \'#{self.name}.#{ __method__ }\' is not defined yet."
+      if settings_for_auto_loading.present?
+        used_part_of_namespace = self.name.gsub( /\A#{ settings_for_auto_loading[ :namespace ].name }/ , "" ).split( "::" ).map { | dir | dir.underscore }
+        ::File.join( settings_for_auto_loading[ :filename ] , *used_part_of_namespace )
+      else
+        raise "This method \'#{self.name}.#{ __method__ }\' is not defined yet."
+      end
     end
 
     def self.other_files
       nil
     end
-
-    class << self
-      alias :required_files :files
+    
+    def self.ignored_files
+      nil
     end
 
     def self.all_files_under_the_top_namespace
@@ -74,6 +127,18 @@ module RequiredFiles
     def self.files_starting_with( *dir_root )
       d_root = dir_root.flatten
       [ ::File.join( *dir_root ) ] + ::Dir.glob( "#{ d_root.join( "/" ) }/**/**.rb" ).sort
+    end
+
+    class << self
+
+      alias :required_files :files
+      
+      private
+      
+      def settings_for_auto_loading
+        nil
+      end
+      
     end
 
   end
--- spec/required_files_spec.rb
+++ spec/required_files_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe RequiredFiles do
   it 'has a version number' do
-    expect(RequiredFiles::VERSION).not_to be nil
+    expect(RequiredFiles::VERSION).to eq( "0.2.0" )
   end
 
   it 'does something useful' do
