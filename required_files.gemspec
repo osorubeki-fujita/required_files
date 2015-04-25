f03357b5657290a88d8a2f6ab96a2ec862192027
Shu Foujita <osorubeki.fujita@gmail.com>
2015-04-25 11:04:19 +0900

    20150425_110403 Installing 0.2.2

--- required_files.gemspec
+++ required_files.gemspec
@@ -32,5 +32,8 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec"
 
+  spec.add_development_dependency "capistrano"
+  spec.add_development_dependency "deployer", ">= 0.1.5"
+
   spec.add_runtime_dependency "activesupport" , ">= 4.2.1"
 end
--- spec/required_files_spec.rb
+++ spec/required_files_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
+require 'deployer'
+
+spec_filename = ::File.expand_path( ::File.dirname( __FILE__ ) )
+version = "0.2.2"
 
 describe RequiredFiles do
-  it 'has a version number' do
-    expect(RequiredFiles::VERSION).to eq( "0.2.0" )
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it "has a version number \'#{ version }\'" do
+    expect( RequiredFiles::VERSION ).to eq( version )
+    expect( ::Deployer.version_check( RequiredFiles::VERSION , spec_filename ) ).to eq( true )
   end
 end
