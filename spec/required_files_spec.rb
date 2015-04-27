ad030339330d9b1a3b9d20ae37dadd9b4e032f4e
Shu Foujita <osorubeki.fujita@gmail.com>
2015-04-27 10:12:25 +0900

    20150427_101224 Install Gem (0.2.5)

--- spec/required_files_spec.rb
+++ spec/required_files_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
-require 'deployer'
+require 'deplo'
 
 spec_filename = ::File.expand_path( ::File.dirname( __FILE__ ) )
-version = "0.2.4"
+version = "0.2.5"
 
 describe RequiredFiles do
   it "has a version number \'#{ version }\'" do
     expect( RequiredFiles::VERSION ).to eq( version )
-    expect( ::Deployer.version_check( RequiredFiles::VERSION , spec_filename ) ).to eq( true )
+    expect( ::Deplo.version_check( RequiredFiles::VERSION , spec_filename ) ).to eq( true )
   end
 end
