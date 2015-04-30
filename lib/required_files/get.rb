c027f9aa0dce215312674f0b3601b862c4ef9075
Shu Fujita <osorubeki.fujita@gmail.com>
2015-04-30 13:53:34 +0900

    20150430_135334 Install Gem (0.2.6)

--- lib/required_files/get.rb
+++ lib/required_files/get.rb
@@ -1,6 +1,6 @@
 module RequiredFiles::Get
 
-  def self.txt( required_files , top_dir , filename = "required_files" )
+  def self.from_txt( required_files , top_dir , filename = "required_files" )
     _required_files = required_files.map { | str | str.gsub( "#{ top_dir }/" , "" ) + ".rb" }
     ::File.open( "#{ top_dir }/#{ filename }.#{ __method__ }" , "w:utf-8" ) do |f|
       f.print _required_files.join( "\n" )
--- lib/required_files/make.rb
+++ lib/required_files/make.rb
@@ -1,6 +1,6 @@
 module RequiredFiles::Make
 
-  def self.from_txt( top_dir , filename = "required_files" )
+  def self.txt( top_dir , filename = "required_files" )
     ::File.open( "#{ top_dir }/#{ filename }.txt" , "r:utf-8" ).read.split( /\n/ ).map { |f| "#{ top_dir }/#{ f }" }
   end
   
