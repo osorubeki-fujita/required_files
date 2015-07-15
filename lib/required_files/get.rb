483b687ecdc6c9906d8b894cbc34cab113ddb3c2
Shu Fujita <osorubeki.fujita@gmail.com>
2015-07-16 01:42:05 +0900

    20150716_014200 Version Up

--- lib/required_files/get.rb
+++ lib/required_files/get.rb
@@ -1,7 +1,9 @@
 module RequiredFiles::Get
 
   def self.from_txt( top_dir , filename = "required_files" )
-    ::File.open( "#{ top_dir }/#{ filename }.txt" , "r:utf-8" ).read.split( /\n/ ).map { |f| "#{ top_dir }/#{ f }" }
+    ::File.open( "#{ top_dir }/#{ filename }.txt" , "r:utf-8" ).read.split( /\n/ ).map { |f|
+      "#{ top_dir }/#{ f }"
+    }
   end
 
 =begin
@@ -10,11 +12,11 @@ module RequiredFiles::Get
       from_hash( key , value )
     }
   end
-  
+
   class << self
-  
+
     private
-  
+
     def from_hash( key , value )
       ary = ::Array.new
       if value.instance_of?( ::Array ) or value.instance_of?( ::String )
@@ -29,7 +31,7 @@ module RequiredFiles::Get
       ary
     end
 
-  end 
+  end
 =end
 
 end
--- spec/required_files_spec.rb
+++ spec/required_files_spec.rb
@@ -2,11 +2,9 @@ require 'spec_helper'
 require 'deplo'
 
 spec_filename = ::File.expand_path( ::File.dirname( __FILE__ ) )
-version = "0.2.6"
 
 describe RequiredFiles do
   it "has a version number \'#{ version }\'" do
-    expect( RequiredFiles::VERSION ).to eq( version )
-    expect( ::Deplo.version_check( RequiredFiles::VERSION , spec_filename ) ).to eq( true )
+    expect( ::Deplo.version_check( ::RequiredFiles::VERSION , spec_filename ) ).to eq( true )
   end
 end
